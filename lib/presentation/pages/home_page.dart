a02287c8a32b75a273b88c79bdd00e4717aa4068
Dwi Supartama <dwisupartama3@gmail.com>
2022-06-04 22:44:25 +0800

    Menambahkan header home page

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:yess_nutrition/common/styles/color_scheme.dart';
 import 'package:yess_nutrition/common/utils/state_enum.dart';
 import 'package:yess_nutrition/presentation/providers/bottom_navigation_bar_notifier.dart';
 import 'package:yess_nutrition/presentation/widgets/custom_button_navigation_bar.dart';
@@ -12,21 +15,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  late MenuNavBar _selectedPage;
-
   @override
   Widget build(BuildContext context) {
-    _selectedPage =
-        Provider.of<BottomNavigationBarNotifier>(context, listen: false)
-            .selectedMenu;
     return Scaffold(
-      bottomNavigationBar: CustomButtonNavigationBar(),
+      bottomNavigationBar: const CustomButtonNavigationBar(),
       body: Consumer<BottomNavigationBarNotifier>(
           builder: (context, result, child) {
         if (result.selectedMenu == MenuNavBar.Home) {
-          return const Center(
-            child: Text("Ini Halaman Home"),
-          );
+          return const BodyHomePage();
         } else if (result.selectedMenu == MenuNavBar.NutriTime) {
           return const Center(
             child: Text("Ini Halaman NutriTime"),
@@ -45,6 +41,118 @@ class _HomePageState extends State<HomePage> {
           );
         }
       }),
+    );
+  }
+}
+
+class BodyHomePage extends StatelessWidget {
+  const BodyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Stack(
+          alignment: AlignmentDirectional.topStart,
+          clipBehavior: Clip.none,
+          children: <Widget>[
+            SvgPicture.asset(
+              'assets/svg/header_background.svg',
+              alignment: Alignment.topCenter,
+              fit: BoxFit.fitWidth,
+              width: MediaQuery.of(context).size.width,
+            ),
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 36,
+                ),
+                child: Row(
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          margin: const EdgeInsets.only(right: 12),
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8),
+                            ),
+                          ),
+                          clipBehavior: Clip.hardEdge,
+                          child: Image.asset(
+                            'assets/img/test_avatar.png',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const Text(
+                              "Hai, Brandon!",
+                              style: TextStyle(
+                                color: primaryBackgroundColor,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 4,
+                            ),
+                            Text(
+                              "Selamat Datang",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .caption
+                                  ?.copyWith(color: primaryBackgroundColor),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const Spacer(),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        width: 44,
+                        height: 44,
+                        decoration: const BoxDecoration(
+                          color: primaryColor,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(12),
+                          ),
+                        ),
+                        child: const Icon(
+                          Icons.settings_outlined,
+                          color: primaryBackgroundColor,
+                          size: 22,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        Positioned(
+          top: 140,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 200,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(24),
+                topRight: Radius.circular(24),
+              ),
+              color: scaffoldBackgroundColor,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
