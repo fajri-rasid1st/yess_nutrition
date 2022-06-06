a2f46913efefefbdf69898e7544438423682443b
aririfqi <27.aririfqi@gmail.com>
2022-06-06 10:34:40 +0800

    menambahkan card waktu makan pada fitur nutritime

--- lib/presentation/pages/nutri_time_page.dart
+++ lib/presentation/pages/nutri_time_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yess_nutrition/data/models/data_menu_makan_nutritime.dart';
 import 'package:yess_nutrition/presentation/pages/home_page.dart';
 
 import 'nutri_time_add_waktu_makan.dart';
@@ -45,6 +46,70 @@ class _NutriTimePagePageState extends State<NutriTimePage> {
               },
             ),
           ],
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 10.0, left: 10.0),
+          child: Row(children: [
+            Text(
+              'Waktu Makan',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ]),
+        ),
+        Container(
+          child: Row(
+            children: [
+              Expanded(
+                  child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.55,
+                child: Container(
+                  child: ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    itemBuilder: (context, index) {
+                      final DataMenuMakan menuMakan = dataWaktuMakan[index];
+                      return InkWell(
+                        onTap: () {},
+                        child: Card(
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Expanded(
+                                flex: 2,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        menuMakan.waktuMakan,
+                                        style: TextStyle(
+                                          fontSize: 16.0,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 10,
+                                      ),
+                                      Text(menuMakan.info),
+                                    ],
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                    itemCount: dataWaktuMakan.length,
+                  ),
+                ),
+              ))
+            ],
+          ),
         ),
       ]),
     );
