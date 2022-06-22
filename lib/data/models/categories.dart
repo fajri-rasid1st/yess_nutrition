a0d5c56fb08db602094780aee78bdff6166056c1
nuraeni28 <nuraeniexecutive18@gmail.com@gmail.com>
2022-06-22 08:49:45 +0800

    menambahkan kategori makanan dan minuman

--- lib/data/models/categories.dart
+++ lib/data/models/categories.dart
@@ -6,16 +6,19 @@ class CategoriKesehatan {
 }
 
 List<CategoriKesehatan> categoriKesehatan = [
-  CategoriKesehatan(title: 'Suplemen Diet', img: 'assets/img/suplement.png'),
+  CategoriKesehatan(title: 'Alat Kesehatan', img: 'assets/img/alat-medis.png'),
   CategoriKesehatan(
       title: 'Essential Oil', img: 'assets/img/essential-oil.png'),
+  CategoriKesehatan(title: 'Masker', img: 'assets/img/masker.png'),
+  CategoriKesehatan(
+      title: 'Kesehatan Mata', img: 'assets/img/kesehatan-mata.png'),
+  CategoriKesehatan(
+      title: 'Kesehatan Mulut & Gigi', img: 'assets/img/kesehatan-mulut.png'),
+  CategoriKesehatan(
+      title: 'Kesehatan Telinga', img: 'assets/img/kesehatan-telinga.png'),
   CategoriKesehatan(
       title: 'Kesehatan Wanita', img: 'assets/img/kesehatan-wanita.png'),
-  CategoriKesehatan(
-      title: 'Vitamin & Multivitamin', img: 'assets/img/vitamin.png'),
-  CategoriKesehatan(title: 'Obat-Obatan', img: 'assets/img/obat.png'),
-  CategoriKesehatan(title: 'Alat Medis', img: 'assets/img/alat-medis.png'),
-  CategoriKesehatan(title: 'Masker', img: 'assets/img/masker.png'),
+  CategoriKesehatan(title: 'Obat & Vitamin', img: 'assets/img/vitamin.png'),
 ];
 
 class CategoriMakananMinuman {
@@ -27,12 +30,14 @@ class CategoriMakananMinuman {
 
 List<CategoriMakananMinuman> categoriMakananMinuman = [
   CategoriMakananMinuman(title: 'Bahan Kue', img: 'assets/img/tepung.png'),
+  CategoriMakananMinuman(title: 'Buah', img: 'assets/img/buah.webp'),
+  CategoriMakananMinuman(title: 'Bumbu', img: 'assets/img/merica.png'),
   CategoriMakananMinuman(title: 'Beras', img: 'assets/img/beras.png'),
-  CategoriMakananMinuman(
-      title: 'Bumbu & Bahan Masakan', img: 'assets/img/merica.png'),
   CategoriMakananMinuman(title: 'Kue', img: 'assets/img/kue.png'),
   CategoriMakananMinuman(
       title: 'Makanan Beku', img: 'assets/img/frozen-food.png'),
   CategoriMakananMinuman(title: 'Makanan Jadi', img: 'assets/img/tumpeng.png'),
-  CategoriMakananMinuman(title: 'Makanan Ringan', img: 'assets/img/snack.png'),
+  CategoriMakananMinuman(title: 'Sayur', img: 'assets/img/sayur.webp'),
+  CategoriMakananMinuman(title: 'Teh', img: 'assets/img/teh.jfif'),
+  CategoriMakananMinuman(title: 'Telur', img: 'assets/img/telur.jpg'),
 ];
